b77850dda263372e75d685d744188210071bb0b7
Jmeas <jellyes2@gmail.com>
2015-05-24 17:16:24 -0400

    Add symbols.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './features/es2015/arrow';
 import './features/es2015/classes';
 import './features/es2015/common-algorithms';
-import './features/es2015/comprehensions';
 import './features/es2015/destructuring';
 import './features/es2015/generators';
 import './features/es2015/iterators';
